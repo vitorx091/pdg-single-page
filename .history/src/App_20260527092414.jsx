b3c31c2a9115ab9c839a1dba56672c6619ca3e65
vitor <117126119+vitorx091@users.noreply.github.com>
2026-05-27 22:37:20 -0300

    Delete .history/src directory

--- .history/src/App_20260527092414.jsx
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>TESTE</h1>
-  )
-}
-
-export default App
--- .history/src/components/layout/Container_20260522225455.jsx
+++
@@ -1,9 +0,0 @@
-function Container({ children }) {
-  return (
-    <div className="w-full max-w-[1200px] mx-auto px-5">
-      {children}
-    </div>
-  )
-}
-
-export default Container
